45e8e90ab9b43578fa1d1b7cbb495936ce0f72df
Md. Anisur Rahman <47313052+anisurrahmanlikhon@users.noreply.github.com>
2024-02-03 14:41:40 +0600

    Update c.js

--- c.js
+++ c.js
@@ -21,7 +21,7 @@ window.addEventListener('load', function() {
   $textarea = document.getElementById('textarea-input');
   $highlight = document.getElementById('highlight-area');
 
-  var code = `// এখানে বাংলা সি কোড লিখো
+  var code = `// এখানে বাংলা সি কোড লেখো
 # অন্তর্ভুক্ত < স্ট্যান্ডার্ড_ইন_আউট.হেডার >
 # অন্তর্ভুক্ত < স্ট্যান্ডার্ড_লাইব্রেরি.হেডার >
 
